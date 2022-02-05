ba696c9ba66776f676bf2b2a0d6beb8c0cceadd5
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2022-02-05 13:56:05 +0530

    Track only cpp files

---
+++ Prologue/datatypes.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a; 
+    float b;
+    char c;
+    bool d;
+    a = 10;
+    b = 3.14;
+    c = 'a';
+    d = false;
+    cout<<"The value of a is: " << a <<'\n';
+    cout<<"The value of b is: " << b <<'\n';
+    cout<<"The value of c is: " << c <<'\n';
+    cout<<"The value of d is: " << d <<'\n';
+    return 0;
+}
